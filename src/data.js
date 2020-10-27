affa99f60d2d177bcedf551ede0cb0f6db9be58d
Sylvia <sylviapap1@gmail.com>
2020-10-27 11:25:19 -0700

    Small data changes

--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 export const posts = [
   {
-    text: "Just gave birth!",
+    text: "Just had a baby!",
     displayName: "Diane Pap",
     username: "mom",
     verified: "verified",
@@ -16,7 +16,7 @@ export const posts = [
     date: "Jan 4, 1995"
   },
   {
-    text: `Sylvia just said her first word at only one month old. Wow! It was "accessibility"`,
+    text: `Sylvia just said her first word! It was "accessibility"`,
     displayName: "Diane Pap",
     username: "mom",
     verified: "verified",
@@ -25,73 +25,79 @@ export const posts = [
   },
   {
     text: "hello world",
-    displayName: "Sylvia Pap",
-    username: "sylviapapdev",
-    verified: "verified",
+    displayName: "Sylvia",
+    username: "sylviapap",
     avatar: "babycomp.jpeg",
     date: "Jul 4, 1995"
   },
   {
     text: "im baby",
-    displayName: "Sylvia Pap",
-    username: "sylviapapdev",
-    verified: "verified",
+    displayName: "Sylvia",
+    username: "sylviapap",
     avatar: "babycomp.jpeg",
     date: "Jul 4, 1995"
   },
   {
-    text: "you know, im somewhat of a hacker myself *types rosebud into the sims 1*",
-    displayName: "Sylvia Pap",
-    username: "sylviapapdev",
+    text: "i am a child computer prodigy\n*types rosebud into the sims 1*",
+    displayName: "Sylvia",
+    username: "sylviapap",
     verified: "verified",
     avatar: "kid.jpg",
     date: "Feb 4, 2000"
   },
   {
-    text: "learning HTML on a little site called Neopets - so fun!",
-    displayName: "Sylvia Pap",
-    username: "sylviapapdev",
+    text: "in 20 years im going to realize that loving cheat codes/guides to games means im going to be very good at reading API documentation *big wink*",
+    displayName: "Sylvia",
+    username: "sylviapap",
+    verified: "verified",
+    avatar: "kid.jpg",
+    date: "Feb 4, 2000"
+  },
+  {
+    text: "learning HTML on Neopets - so fun!",
+    displayName: "Sylvia",
+    username: "sylviapap",
     verified: "verified",
     avatar: "kid.jpg",
     date: "Nov 1, 2004"
   },
   {
     text: "me explaining neopets to my parents and other children in 4th grade",
-    displayName: "Sylvia Pap",
-    username: "sylviapapdev",
+    displayName: "Sylvia",
+    username: "sylviapap",
     verified: "verified",
     avatar: "kid.jpg",
-    date: "Nov 2, 2004",
+    date: "Dec 12, 2004",
     image: "https://i.kym-cdn.com/entries/icons/facebook/000/022/524/tumblr_o16n2kBlpX1ta3qyvo1_1280.jpg"
   },
   {
-    text: "neopets is so fun, i love clicking 'view page source' on the internet to solve all their puzzles. but it doesnt seem like something that normal kids do so i think ill give it up for now. oh well!",
-    displayName: "Sylvia Pap",
-    username: "sylviapapdev",
+    text: "i love neopets and puzzles! today i learned how to 'view page source'\nbut it doesnt seem like something that normal kids do so i think ill give it up for now. oh well!",
+    displayName: "Sylvia",
+    username: "sylviapap",
     verified: "verified",
     avatar: "kid.jpg",
     date: "Jan 1, 2005"
   },
   {
-    text: "html strikes again. obsessed with myspace and making my own themes. if only there was a way to do this in school?",
-    displayName: "Sylvia Pap",
-    username: "sylviapapdev",
+    text: "html strikes again. who knew it would be so useful for making my own pages on myspace? if only i could spend this much time on something for school",
+    displayName: "Sylvia",
+    username: "sylviapap",
     verified: "verified",
     avatar: "icarly.png",
     date: "Sep 1, 2007"
   },
   {
-    text: "i like math and science. i wish we had ap computer science here. today some guys in math were talking about computer 'languages' and i had no idea what that meant",
-    displayName: "Sylvia Pap",
-    username: "sylviapapdev",
+    text: "i like math and science. i wish we had AP computer science. today some guys in class were talking about computer 'languages' and i had no idea what that meant",
+    displayName: "Sylvia",
+    username: "sylviapap",
     verified: "verified",
     avatar: "icarly.png",
     date: "Nov 23, 2009"
   },
   {
     text: "my high school giving us one class called 'computer applications' where we learn how to use SUM in excel while there are 10 different classes for FFA and agricultural science",
-    displayName: "Sylvia Pap",
-    username: "sylviapapdev",
+    displayName: "Sylvia",
+    username: "sylviapap",
     verified: "verified",
     avatar: "icarly.png",
     date: "Jan 1, 2010",
@@ -99,16 +105,16 @@ export const posts = [
   },
   {
     text: "college! yay!",
-    displayName: "Sylvia Pap",
-    username: "sylviapapdev",
+    displayName: "Sylvia",
+    username: "sylviapap",
     verified: "verified",
     avatar: "college1.jpg",
     date: "Sep 22, 2013"
   },
   {
     text: "spoke too soon",
-    displayName: "Sylvia Pap",
-    username: "sylviapapdev",
+    displayName: "Sylvia",
+    username: "sylviapap",
     verified: "verified",
     avatar: "college.png",
     date: "Sep 23, 2013",
@@ -116,65 +122,58 @@ export const posts = [
   },
   {
     text: "me in comp sci 101, day 1 lecture 1",
-    displayName: "Sylvia Pap",
-    username: "sylviapapdev",
+    displayName: "Sylvia",
+    username: "sylviapap",
     verified: "verified",
     avatar: "college.png",
     date: "Mar 20, 2014",
     image: "https://media1.tenor.com/images/96e316a78baaad3ee5b79fa0c6d4f7e6/tenor.gif?itemid=9777772"
   },
   {
-    text: "bold of you to assume that i know what a java variable is, professor computer man",
-    displayName: "Sylvia Pap",
-    username: "sylviapapdev",
+    text: "professor: java is an essential foundation to the study of computer science\nme: im going to need a different kind of JAVA to get through this morning haha\nprofessor: \nkid next to me already applying to google internships: \nme:",
+    displayName: "Sylvia",
+    username: "sylviapap",
     verified: "verified",
     avatar: "college.png",
-    date: "Mar 21, 2014"
-  },
-  {
-    text: "bold of you to assume that i know what a terminal is, skinny white boy sitting next to me",
-    displayName: "Sylvia Pap",
-    username: "sylviapapdev",
-    verified: "verified",
-    avatar: "college.png",
-    date: "Mar 22, 2014"
+    date: "Mar 21, 2014",
+    image: "https://i.pinimg.com/564x/22/c3/94/22c3941a93a3578b56b4a5a539b5a4f8.jpg"
   },
   {
     text: "i would like to inform the haters that i have dropped comp sci 101",
-    displayName: "Sylvia Pap",
-    username: "sylviapapdev",
+    displayName: "Sylvia",
+    username: "sylviapap",
     verified: "verified",
     avatar: "college.png",
     date: "Mar 23, 2014"
   },
   {
     text: "might just f around and major in philosophy",
-    displayName: "Sylvia Pap",
-    username: "sylviapapdev",
+    displayName: "Sylvia",
+    username: "sylviapap",
     verified: "verified",
     avatar: "phil.png",
     date: "Jan 1, 2016"
   },
   {
     text: "ok guess im going to dedicate my life to politics, human rights law, and apocalypse prepping",
-    displayName: "Sylvia Pap",
-    username: "sylviapapdev",
+    displayName: "Sylvia",
+    username: "sylviapap",
     verified: "verified",
     avatar: "phil.png",
     date: "Nov 8, 2016"
   },
   {
     text: `me: why is my favorite class 'logic' and why didnt i stick with the math major? 'a if and only if b,' am i right? \n them: ma'am, this is a wendys`,
-    displayName: "Sylvia Pap",
-    username: "sylviapapdev",
+    displayName: "Sylvia",
+    username: "sylviapap",
     verified: "verified",
     avatar: "phil.png",
     date: "Jan 15, 2017"
   },
   {
     text: "yesterday, undergrad. today, paralegal. tomorrow, ",
-    displayName: "Sylvia Pap",
-    username: "sylviapapdev",
+    displayName: "Sylvia",
+    username: "sylviapap",
     verified: "verified",
     avatar: "business.png",
     date: "Aug 1, 2017",
@@ -182,8 +181,8 @@ export const posts = [
   },
   {
     text: "me after watching one video on cryptography on khan academy",
-    displayName: "Sylvia Pap",
-    username: "sylviapapdev",
+    displayName: "Sylvia",
+    username: "sylviapap",
     verified: "verified",
     avatar: "business.png",
     date: "Jan 29, 2018",
@@ -191,90 +190,91 @@ export const posts = [
   },
   {
     text: "yall mind if i... quit my job and go back to school",
-    displayName: "Sylvia Pap",
-    username: "sylviapapdev",
+    displayName: "Sylvia",
+    username: "sylviapap",
     verified: "verified",
     avatar: "business.png",
     date: "Mar 5, 2018"
   },
   {
-    text: "those who do not study history are doomed to probably be pretty happy actually because history is pretty f*ing horrible why am i here",
-    displayName: "Sylvia Pap",
-    username: "sylviapapdev",
+    text: "those who do not study history are doomed to probably be pretty happy actually because history is horrible why am i here",
+    displayName: "Sylvia",
+    username: "sylviapap",
     verified: "verified",
     avatar: "business.png",
     date: "Sep 21, 2018"
   },
   {
-    text: "finishing a masters in IR in london then moving to mexico city to teach math to kids like",
-    displayName: "Sylvia Pap",
-    username: "sylviapapdev",
+    text: "finishing an IR masters in london then moving to mexico to teach math like",
+    displayName: "Sylvia",
+    username: "sylviapap",
     verified: "verified",
     avatar: "business.png",
     date: "Oct 1, 2019",
     image: "meme.jpg"
   },
   {
-    text: "silicon valley is a good show",
-    displayName: "Sylvia Pap",
-    username: "sylviapapdev",
+    text: "why do i love silicon valley (the show) so much",
+    displayName: "Sylvia",
+    username: "sylviapap",
     verified: "verified",
     avatar: "business.png",
     date: "Oct 15, 2019"
   },
   {
     text: "`hello world` my old friend",
-    displayName: "Sylvia Pap",
-    username: "sylviapapdev",
+    displayName: "Sylvia",
+    username: "sylviapap",
     verified: "verified",
     avatar: "business.png",
     date: "Oct 20, 2019"
   },
   {
-    text: "how to declare a variable in javascript? havent heard that name in years",
-    displayName: "Sylvia Pap",
-    username: "sylviapapdev",
+    text: "khan academy intro to html & javascript? havent heard that name in years",
+    displayName: "Sylvia",
+    username: "sylviapap",
     verified: "verified",
     avatar: "business.png",
     date: "Oct 21, 2019"
   },
   {
     text: "ok, ill bite. what is 'imposter syndrome'",
-    displayName: "Sylvia Pap",
-    username: "sylviapapdev",
+    displayName: "Sylvia",
+    username: "sylviapap",
     verified: "verified",
     avatar: "business.png",
     date: "Oct 22, 2019"
   },
   {
     text: "id like to file a class action lawsuit against my own brain for keeping me away from tech for so long",
-    displayName: "Sylvia Pap",
-    username: "sylviapapdev",
+    displayName: "Sylvia",
+    username: "sylviapap",
     verified: "verified",
     avatar: "business.png",
     date: "Oct 23, 2019"
   },
   {
-    text: "just did a thing on my terminal that made 'hello world' show up on my browser so i think its safe to say there's no turning back",
-    displayName: "Sylvia Pap",
-    username: "sylviapapdev",
+    text: "just typed into terminal and it showed up on the browser so i think its safe to say",
+    displayName: "Sylvia",
+    username: "sylviapap",
     verified: "verified",
     avatar: "business.png",
-    date: "Oct 29, 2019"
+    date: "Oct 29, 2019",
+    image: "https://i.kym-cdn.com/entries/icons/mobile/000/025/351/afoeeee.jpg"
   },
   {
     text: `career quiz: u should be computer scientist\nme: haha no im dumb\n school: no ur pretty good at STEM\nme: i wasnt top of my classes tho and computers are a whole other thing\nkhan academy, coursera, udacity, youtube, silicon valley: we are all living rent free in your brain \n
     me: wait`,
-    displayName: "Sylvia Pap",
-    username: "sylviapapdev",
+    displayName: "Sylvia",
+    username: "sylviapap",
     verified: "verified",
     avatar: "business.png",
     date: "Nov 6, 2019"
   },
   {
     text: "explaining to friends and family why i have decided to go back to yet another form of school and do a coding bootcamp\nmy parents, supporting whatever i do as long as i still help with the printer",
-    displayName: "Sylvia Pap",
-    username: "sylviapapdev",
+    displayName: "Sylvia",
+    username: "sylviapap",
     verified: "verified",
     avatar: "prof.jpeg",
     date: "Dec 1, 2019",
@@ -282,15 +282,15 @@ export const posts = [
   },
   {
     text: "wow i am absolutely in love with my coding bootcamp, the city of san francisco, and this wework building. sure hope nothing major happens in the next couple of months to disrupt this exciting city tech life",
-    displayName: "Sylvia Pap",
-    username: "sylviapapdev",
+    displayName: "Sylvia",
+    username: "sylviapap",
     verified: "verified",
     avatar: "prof.jpeg",
     date: "Jan 27, 2020"
   },
   {
     text: "in all seriousness, if you've made it this far, thank you. coding is my passion, something ive never been able to say before (not since neopets at least) and im obsessed with twitter. i really hope this little clone project shows a bit of that. send tweet",
-    displayName: "Sylvia Pap",
+    displayName: "Sylvia",
     username: "sylviapapdev",
     verified: "verified",
     avatar: "prof.jpeg",
